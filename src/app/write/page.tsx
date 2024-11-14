defb2ece971dc16cd5a4b55c2c0e392162094302
Josué Salazaku <josuesalazaku@gmail.com>
2024-11-14 23:53:26 +0100

    Refactor: Remove Clerk authentication components from HomePage and WritePage

--- src/app/write/page.tsx
+++ src/app/write/page.tsx
@@ -1,20 +1,16 @@
-import { SignedIn, SignedOut } from '@clerk/nextjs'
+
 import React from 'react'
 import CreatePost from '~/components/createPost'
 
 export default function page() {
   return (
     <main className="h-screen flex flex-col items-center justify-start bg-primary text-white">
-    <SignedIn>
       <section>
         <CreatePost />
       </section>
-    </SignedIn>
-    <SignedOut>
       <section>
         <p>Please sign in to create a post.</p>
       </section>
-    </SignedOut>
   </main>
   )
 }
