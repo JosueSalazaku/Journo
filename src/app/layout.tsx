bc1b677249bf91e1cd0f4d1766cc8cbe6e907d4d
Josué Salazaku <josuesalazaku@gmail.com>
2024-11-14 18:22:34 +0100

    Replace ClerkProvider with SessionProvider in layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
-import { ClerkProvider } from '@clerk/nextjs';
 import { TopNav } from "~/components/TopNav";
 import "~/styles/globals.css";
+import { SessionProvider } from "next-auth/react"
 
 export const metadata = {
   title: "Journo",
@@ -16,12 +16,12 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className="bg-primary font-noto">
-        <ClerkProvider>
+        <SessionProvider>
           <div className="max-w-600 mx-auto h-[100vh]">
             <TopNav />
             {children}
           </div>
-        </ClerkProvider>
+        </SessionProvider>
       </body>
     </html>
   );
--- src/auth.ts
+++ src/auth.ts
@@ -1,6 +1,9 @@
 import NextAuth from "next-auth"
 import Google from "next-auth/providers/google"
+import Notion from "next-auth/providers/notion"
+
  
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [Google],
+  providers: [Google, Notion]
+
 })
