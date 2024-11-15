4a4d3bb0032576fd77936bc9cdc0ab1dabf0b523
Josué Salazaku <josuesalazaku@gmail.com>
2024-11-15 22:39:05 +0100

    Refactor: Simplify login page layout, remove unused pages, and enhance navigation links

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,21 +1,10 @@
-import { Input } from "~/components/ui/input";
-import { login, signup } from "./actions";
-import { Button } from "~/components/ui/button";
 import { OAuthButtons } from "./oauth-signin";
 
 export default function LoginPage() {
   return (
-      <main className="h-screen flex flex-col items-center justify-start text-white p-4 sm:p-8">
+      <main className="h-[900px] w-full flex flex-col items-center justify-center text-white sm:p-8">
           <OAuthButtons />
-      <form className="w-96">
-        <label htmlFor="email">Email:</label>
-        <Input id="email" name="email" type="email" required />
-        <label htmlFor="password">Password:</label>
-        <Input id="password" name="password" type="password" required />
-        <Button formAction={login}>Log in</Button>
-        <Button formAction={signup}>Sign up</Button>
-      </form>
-    </main>
+      </main>
   );
 }
  
