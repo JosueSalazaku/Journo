9a22c8985f66f11b4da3ed45bfb0b741007a3668
Josué Salazaku <josuesalazaku@gmail.com>
2024-11-15 23:37:04 +0100

    Create dropdown menu #6

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,28 +1,31 @@
 "use client";
 import Link from "next/link";
-import { useEffect, useState } from "react";
+import { useState } from "react";
+import { IoIosMenu } from "react-icons/io";
 
 export function Nav() {
-  
-  const [open, setOpen] = useState(false)
-
-  useEffect(() => {
-    function toggle() {
-      // func
-    }
-    }, [])
+  const [open, setOpen] = useState<boolean>(false);
 
   return (
-
-<nav className="flex h-20 w-full items-center justify-between bg-primary px-5">
-  <div className="flex w-full justify-between items-center gap-3 text-2xl font-bold text-white">
-    <Link href="/">Journo</Link>
-    <div className="hidden md:flex justify-end items-center gap-3 text-lg font-bold text-white">
-      <Link href="/write">Write</Link>
-      <Link href="/explore">Explore</Link>
-      <Link href="/insights">Insights</Link>
-    </div>
-  </div>
-</nav>
+    <nav className="flex h-20 w-full items-center justify-between bg-primary px-5">
+      <div className="flex w-full items-center justify-between gap-3 text-2xl font-bold text-white">
+        <Link href="/">Journo</Link>
+        <button onClick={() => setOpen(!open)}><IoIosMenu /></button>
+        {open && (
+            <div className="absolute top-20 right-0 bg-black h-full w-full">
+            <ul className="flex flex-col items-start p-3">
+              <li className="py-2"><Link href="/write">Write</Link></li>
+              <li className="py-2"><Link href="/explore">Explore</Link></li>
+              <li className="py-2"><Link href="/insights">Insights</Link></li>
+            </ul>
+            </div>
+        )}
+        <div className="hidden items-center justify-end gap-3 text-lg font-bold text-white md:flex">
+          <Link href="/write">Write</Link>
+          <Link href="/explore">Explore</Link>
+          <Link href="/insights">Insights</Link>
+        </div>
+      </div>
+    </nav>
   );
 }
