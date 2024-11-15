502e149a8c3cf329cc594937f4e3d70ec2424d5c
Josué Salazaku <josuesalazaku@gmail.com>
2024-11-15 13:21:28 +0100

    Refactor: Update database connection to include schema export and streamline initialization #1

--- src/server/db/index.ts
+++ src/server/db/index.ts
@@ -1,13 +1,7 @@
-import { drizzle} from 'drizzle-orm/postgres-js'
-import postgres from 'postgres'
+import { drizzle } from "drizzle-orm/postgres-js";
+import postgres from "postgres";
+import * as schema from "./schema";
 
-
-const connectionString = process.env.DATABASE_URL
-
-if (!connectionString) {
-  throw new Error('DATABASE_URL is not defined');
-}
-
-const client = postgres(connectionString, { prepare: false })
-export const db = drizzle(client);
-
+const connection = postgres(process.env.DATABASE_URL!, { prepare: false });
+export const db = drizzle(connection, { schema });
+export * from "./schema";
