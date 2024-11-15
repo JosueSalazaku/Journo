a87daaa369f5b338fad9f26744e2072b36331b39
Josué Salazaku <josuesalazaku@gmail.com>
2024-11-15 17:35:08 +0100

    Refactor: Add Notion OAuth button and update button styling #2

--- src/app/login/oauth-signin.tsx
+++ src/app/login/oauth-signin.tsx
@@ -14,20 +14,25 @@ export function OAuthButtons() {
       displayName: "Google",
       icon: <Image src="google.svg" alt="Google" height={20} width={20} />,
     },
+    {
+      name: "notion",
+      displayName: "Notion",
+      icon: <Image src="notion.svg" alt="Notion" height={20} width={20} />, 
+    }
   ];
 
   return (
     <>
       {oAuthProviders.map((provider) => (
+        <div key={provider.name} className="mb-4">
         <button
-          key={provider.name}
-          className="flex w-72 h-12 items-center justify-center gap-1 rounded-xl bg-slate-900 p-4 hover:bg-slate-800"
+          className="flex w-72 h-12 items-center justify-center gap-1 rounded-xl bg-slate-900 p-6 hover:bg-slate-800"
           >
           {provider.icon}    
           <h1>Sign in with</h1>
           {provider.displayName}
-          
         </button>
+        </div>
       ))}
     </>
   );
