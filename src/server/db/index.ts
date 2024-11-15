a0323b6ed357af47b8d9186c72ba7b980b96cb8e
Josué Salazaku <josuesalazaku@gmail.com>
2024-11-15 12:16:05 +0100

    Refactor: Update database query to include user selection by ID

--- src/server/db/index.ts
+++ src/server/db/index.ts
@@ -1,6 +1,7 @@
-import { drizzle } from 'drizzle-orm/postgres-js'
+import { drizzle} from 'drizzle-orm/postgres-js'
 import postgres from 'postgres'
 import { users } from './schema'
+import { eq } from 'drizzle-orm';
 
 const connectionString = process.env.DATABASE_URL
 
@@ -12,4 +13,5 @@ const client = postgres(connectionString, { prepare: false })
 export const db = drizzle(client);
 
 const allUsers = await db.select().from(users);
+const userById = await db.select().from(users).where(eq(users.id, ''));
         
