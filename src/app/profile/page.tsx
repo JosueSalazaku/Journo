c729fe16a8cbe73defa02647933609336c23f710
Josué Salazaku <josuesalazaku@gmail.com>
2024-11-21 17:15:09 +0100

    refactor: Integrate SessionProvider and implement user session handling in profile page #2

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,9 +1,25 @@
+"use client"
 import React from 'react'
+import { useCustomSession } from '~/components/SessionProvider'
 
-function page() {
+export default function Page() {
+
+const session = useCustomSession()
+
+  if (!session.data?.user) {
+    return (
+      <div>You are not logged in</div>
+    )
+  } else if (session.data.user) {
+
+    const { name, email, image } = session.data.user;
+
+    return (
+      <div> Welcome back {name}</div>
+    )
+  }
+
   return (
     <div>page</div>
   )
 }
-
-export default page
