a39bfcaa4b37b73845a3228d1f19bc07bfadb56d
Josué Salazaku <josuesalazaku@gmail.com>
2024-11-21 15:29:16 +0100

    refactor: Update authentication schema and user model to enhance database structure and improve user management

--- src/app/api/users/route.ts
+++ src/app/api/users/route.ts
@@ -1,43 +1,13 @@
 import { NextResponse } from "next/server";
 import { db } from "~/server/db"; 
-import { users } from "~/server/db/schema"; 
-import type { User } from "~/types"; 
+import { user } from "~/server/db/auth-schema"; 
 
 export async function GET() {
   try {
-    const data = await db.select().from(users);
+    const data = await db.select().from(user);
     return NextResponse.json(data);
   } catch (error) {
     console.error("Error fetching users:", error);
     return NextResponse.json({ error: "Error fetching users" }, { status: 500 });
-  }
-}
-
-
-export async function POST(request: Request) {
-  try {
-    // Validate and parse the request body
-    const user: User = await request.json() as User;
-
-    // Validate incoming data before inserting into the database
-    if (!user.name || !user.email || !user.picture) {
-      return NextResponse.json({ error: "Missing required fields" }, { status: 400 });
-    }
-    // Insert user into the database
-    const result = await db
-      .insert(users)
-      .values({
-        id: user.id,
-        name: user.name,
-        email: user.email,
-        picture: user.picture,
-        role: user.role
-      })
-
-    // Return the inserted user or success response
-    return NextResponse.json(result);
-  } catch (error) {
-    console.error("Error adding new user:", error);
-    return NextResponse.json({ error: "Error adding new user" }, { status: 500 });
   }
 }
--- src/types.ts
+++ src/types.ts
@@ -1,17 +1,47 @@
 export interface User {
-  id: string;
-  name: string;
-  email: string;
-  picture: string;
-  role: string;
+	id: string;
+	name: string;
+	email: string;
+	emailVerified: boolean;
+	image?: string;
+	createdAt: Date;
+	updatedAt: Date;
 }
 
 export interface Post {
-  id: string;
-  title: string;
-  userId: string;
-  content: string;
-  createdAt: Date;
-  updatedAt: Date;
+	id: string;
+	title: string;
+	content: string;
+	authorId: string;
+	createdAt: Date;
+	updatedAt: Date;
+}
+
+export interface Session {
+	id: string;
+	expiresAt: Date;
+	ipAddress?: string;
+	userAgent?: string;
+	userId: string;
+}
+
+export interface Account {
+	id: string;
+	accountId: string;
+	providerId: string;
+	userId: string;
+	accessToken?: string;
+	refreshToken?: string;
+	idToken?: string;
+	expiresAt?: Date;
+	password?: string;
+}
+
+export interface Verification {
+	id: string;
+	identifier: string;
+	value: string;
+	expiresAt: Date;
+	createdAt?: Date;
 }
 
