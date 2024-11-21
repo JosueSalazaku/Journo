bcc714c03b2567592a3758b2efcb87c91fdf6253
Josué Salazaku <josuesalazaku@gmail.com>
2024-11-21 16:17:13 +0100

    refactor: Add GitHub authentication provider to better-auth configuration

--- lib/auth.ts
+++ lib/auth.ts
@@ -15,10 +15,10 @@ export const auth = betterAuth({
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
       redirectURI: `${process.env.BASE_URL}/api/auth/callback/google`,
     },
-    // github: {
-    //   clientId: process.env.GITHUB_CLIENT_ID!,
-    //   clientSecret: process.env.GITHUB_CLIENT_SECRET!,
-    //   redirectURI: `${process.env.BASE_URL}/api/auth/callback/google`,
-    // },
+    github: {
+      clientId: process.env.GITHUB_CLIENT_ID!,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET!,
+      redirectURI: `${process.env.BASE_URL}/api/auth/callback/github`,
+    },
   },
 });
