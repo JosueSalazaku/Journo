a212ab2094ea256b77169b4e9b91943616fedd22
Josué Salazaku <josuesalazaku@gmail.com>
2025-02-27 00:38:44 +0100

    refactor: Update stroke color and button styles for DarkModeButton component

--- src/components/DarkModeButton.tsx
+++ src/components/DarkModeButton.tsx
@@ -5,12 +5,12 @@ import { Moon, Sun } from "lucide-react";
 function DarkModeButton() {
   const { theme, toggleTheme } = useTheme();
 
-const strokeColor = theme === "dark" ? "#ffffff" : "#020114"; // White for dark mode, black for light mode
+const strokeColor = theme === "dark" ? "#ffffff" : "#2f0659"; // White for dark mode, black for light mode
 
   return (
     <button
       onClick={toggleTheme}
-      className="hover:bg-main-orange fixed bottom-10 right-10 flex items-center justify-center rounded-full border p-4"
+      className="hover:bg-main-orange fixed bottom-10 dark:bg-primary-dark bg-border-light right-10 flex items-center justify-center rounded-full p-4"
     >
       {theme === "dark" ? <Sun stroke={strokeColor} /> : <Moon stroke={strokeColor} />}
     </button>
